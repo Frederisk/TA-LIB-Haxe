081d2cfd19df2792cbeca195b5099ddbbc3ebfc2
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2023-01-03 23:47:45 +0800

    Update Sqrt.hx

--- src/ta/func/Sqrt.hx
+++ src/ta/func/Sqrt.hx
@@ -1,1 +1,42 @@
 package ta.func;
+
+@:keep
+function Sqrt(startIndex:Int, endIndex:Int, inReal:Array<Float>) {
+    var outBegIndex:Int;
+    var outNBElement:Int;
+    var outReal:Array<Float> = [];
+
+    var outIndex:Int;
+    var i:Int;
+
+    if (startIndex < 0) {
+        throw new TAException(OutOfRangeStartIndex);
+    }
+    if (endIndex < 0 || endIndex < startIndex) {
+        throw new TAException(OutOfRangeEndIndex);
+    }
+    if (inReal == null) {
+        throw new TAException(BadParam);
+    }
+
+    i = startIndex;
+    outIndex = 0;
+    while (i <= endIndex) {
+        outReal[outIndex] = Math.sqrt(inReal[i]);
+        i++;
+        outIndex++;
+    }
+
+    outNBElement = outIndex;
+    outBegIndex = startIndex;
+
+    return {
+        outBegIndex: outBegIndex,
+        outNBElement: outNBElement,
+        outReal: outReal
+    };
+}
+
+function SqrtLookback() {
+    return 0;
+}
