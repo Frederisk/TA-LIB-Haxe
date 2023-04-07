ed1f56635a01dd2e7da7c031b9bdcc7ed4f67bf5
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2023-04-07 15:07:22 +0800

    Add candle utility functions

--- src/ta/func/Utility.hx
+++ src/ta/func/Utility.hx
@@ -1,5 +1,8 @@
 package ta.func;
 
+import ta.Globals.RangeType;
+import ta.Globals.CandleSettingType;
+
 inline function RoundPos(x:Float):Float {
     return (Math.ffloor((x) + 0.5));
 }
@@ -34,7 +37,6 @@ inline function IsZeroOrNeg(v:Float):Bool {
 // var prev_#_Even:Float;
 // var prev_#_input_Odd:Float;
 // var prev_#_input_Even:Float;
-
 // INIT_HILBERT_VARIABLES(varName) :
 // #_Odd[0] = 0.0;
 // #_Odd[1] = 0.0;
@@ -47,7 +49,6 @@ inline function IsZeroOrNeg(v:Float):Bool {
 // prev_#_Even = 0.0;
 // prev_#_input_Odd  = 0.0;
 // prev_#_input_Even = 0.0;
-
 // DO_HILBERT_TRANSFORM(varName,input,OddOrEvenId) :
 // hilbertTempReal = a *#2#;
 // #1# = -#1#_#3#[hilbertIdx];
@@ -58,7 +59,6 @@ inline function IsZeroOrNeg(v:Float):Bool {
 // #1#+= prev_#1#_#3#;
 // prev_#1#_input_#3# =#2#;
 // #1#*= adjustedPrevPeriod;
-
 // DO_HILBERT_ODD(varName,input)  DO_HILBERT_TRANSFORM(varName,input,Odd)
 // DO_HILBERT_EVEN(varName,input) DO_HILBERT_TRANSFORM(varName,input,Even)
 
@@ -66,8 +66,37 @@ inline function PerToK(per:Float) {
     return (2.0 / (per + 1));
 }
 
+/**
+ * TA_CANDLERANGE
+ */
+inline function CandleRange(set:CandleSettingType, index:Int, inOpen:Array<Float>, inHigh:Array<Float>, inLow:Array<Float>, inClose:Array<Float>) {
+    return
+    ((Globals.candleSettings[set].rangeType == RangeType.RealBody) ?
+        Math.abs(inClose[index] - inOpen[index]) :
+        ((Globals.candleSettings[set].rangeType == RangeType.HighLow) ?
+            (inHigh[index] - inLow[index]) :
+            ((Globals.candleSettings[set].rangeType == RangeType.Shadows) ?
+                (inHigh[index] - (inClose[index] >= inOpen[index] ? inClose[index] : inOpen[index])) +
+                ((inClose[index] >= inOpen[index] ? inOpen[index] : inClose[index]) - inLow[index]) : 0)));
+}
 
+/**
+ * TA_CANDLEAVERAGE
+ */
+ inline function CandleAverage(set:CandleSettingType, sum:Int, index:Int,inOpen:Array<Float>, inHigh:Array<Float>, inLow:Array<Float>, inClose:Array<Float>){
+    return
+    (Globals.candleSettings[set].factor *
+        (Globals.candleSettings[set].avgPeriod != 0.0 ?
+            sum / Globals.candleSettings[set].avgPeriod :
+            CandleRange(set, index,inOpen, inHigh, inLow, inClose)) /
+        (Globals.candleSettings[set].rangeType == RangeType.Shadows ?
+            2.0 :
+            1.0));
+ }
+
+//////////////////////
 // private function
+//////////////////////
 
 inline function TrueRange(th:Float, tl:Float, yc:Float) {
     var out = th - tl;
